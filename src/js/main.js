51ef406e8ee9214455576eb904977d5766a18d6e
Tanya Kulikovskaya <tatsiana@pedanov.ru>
2021-02-17 15:16:21 +0300

    Connected modal windows

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ const burger = document.querySelector('.header-menu__icon');
 const menu = document.querySelector('.header-menu');
 const body = document.querySelector('body');
 const overlay = document.querySelector('.overlay');
-const menuList = document.querySelectorAll(".header-menu__link > a");
+const menuList = document.querySelectorAll(".header-menu__link a");
 
 
 
@@ -14,10 +14,10 @@ burger.addEventListener('click', function () {
 });
 
 overlay.addEventListener('click', function () {
-    burger.classList.toggle('active');
-    menu.classList.toggle('active');
-    body.classList.toggle('lock');
-    overlay.classList.toggle('show');
+    burger.classList.remove('active');
+    menu.classList.remove('active');
+    body.classList.remove('lock');
+    overlay.classList.remove('show');
 });
 
 
@@ -31,19 +31,66 @@ menuList.forEach(i => {
 });
 
   
-const btn = document.querySelector(".news__btn");
+const btnOpen = document.querySelector(".news__btn");
 const content = document.querySelector(".hidden");
 
-btn.addEventListener("click", btnClick);
+btnOpen.addEventListener("click", btnOpenClick);
 
-function btnClick() {
+function btnOpenClick() {
     console.log(content.classList);
 
     if (content.classList.contains("hidden")) {
-        btn.textContent = "Скрыть все новости";
+        btnOpen.textContent = "Скрыть все новости";
     } else {
-        btn.textContent = "Показать все новости";
+        btnOpen.textContent = "Показать все новости";
     }
 
     content.classList.toggle("hidden");
 }
+
+
+var modal = document.getElementById('modal-take-home');
+var modalShow = document.getElementsByClassName("show")
+var modalClose = document.getElementsByClassName("modal-form__btn--secondary");
+var close = document.getElementsByClassName("close-icon");
+
+
+
+for (var i = 0; i < modalShow.length; i++) {
+	modalShow[i].onclick = function() {
+    var id = this.getAttribute('data-modal');
+    var modal = document.getElementById(id);
+    modal.style.display = 'block';
+  }
+}
+
+for (var i = 0; i < modalClose.length; i++) {
+  modalClose[i].onclick = function() {
+    var id = this.getAttribute('data-modal');
+    var modal = document.getElementById(id);
+    modal.style.display = "none";
+  }
+}
+
+for (var i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var id = this.getAttribute('data-modal');
+
+      
+      var modal = document.getElementById(id);
+      modal.style.display = 'none';
+    }
+  }
+
+
+window.onclick = function(event) {
+  var isModal = (' ' + event.target.className + ' ').indexOf(' modal ') > -1;
+  if (isModal) {
+    event.target.style.display = "none";
+  }
+}
+
+
+
+
+
